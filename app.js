897ee9877e9e1eba762fc4429f785b53b2a6d375
Insha Mahfooz <insha8471@gmail.com>
2024-03-05 01:17:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,111 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btncolor = ["yellow", "green", "red", "purple"];
+
+let started = false;
+let level = 0;
+
+// let gameStatus = document.querySelector('.gameStatus');
+
+let startBtn = document.querySelector("#start-btn");
+
+startBtn.addEventListener("click", function(e) {
+    if(started == false){
+        started = true;
+        levelUp();
+    }
+});
+
+function btnFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function() {
+        btn.classList.remove('flash');
+    }, 400);
+}
+
+let yourscr = document.querySelector(".your-scr");
+let highestScr = document.querySelector(".highest-scr");
+
+function levelUp() {
+    userSeq = [];
+    level++;
+    // gameStatus.innerText = `Level ${level}`;
+
+    yourscr.innerText = `${level}`;
+
+    if(yourscr.innerText > highestScr.innerText){
+        highestScr.innerText = yourscr.innerText;
+        saveData();
+    }
+
+    let randomIndex = Math.floor(Math.random()*4);
+    let randcolor = btncolor[randomIndex];
+    let randBtn = document.querySelector(`.${randcolor}`);
+    gameSeq.push(randcolor);
+    btnFlash(randBtn);
+}
+
+//***************************************************************************************** */
+
+//local storage
+function saveData(){
+    localStorage.setItem("data", highestScr.innerText);
+}
+
+function showTask(){
+    highestScr.innerText = localStorage.getItem("data");
+}
+
+showTask();
+
+//*********************************************************************************** */
+
+function checkAns(idx) {
+    if(gameSeq[idx] === userSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp, 1000);
+        }
+    }else{
+        // gameStatus.innerHTML =  `GAME OVER! Your score was <b> ${level}`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 150)
+        let gameOver = document.querySelector("#game-over");
+        gameOver.style.visibility = "visible";
+        reset();
+    }
+}
+
+
+let backGame = document.querySelector("#back-game");
+backGame.addEventListener("click", ()=> {
+    let gameOver = document.querySelector("#game-over");
+    gameOver.style.visibility = "hidden";
+    reset();
+})
+
+
+function btnPress() {
+    if(started == true){
+        let btn = this;
+        btnFlash(btn);
+        userclr = btn.getAttribute("id");
+        userSeq.push(userclr);
+        checkAns(userSeq.length-1);
+    }
+}
+
+let allBtns = document.querySelectorAll(".btn");
+
+for(btn of allBtns){
+    btn.addEventListener('click', btnPress);
+}
+
+function reset() {
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
+}
